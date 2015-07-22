7dc49a6b4a4771653888383750957a80ee5e414c
Etienne HERMAND <etienneMacBookAir@Etiennes-MacBook-Air.local>
2015-07-22 17:45:39 +0200

    Classes added

---
+++ MySchool/HomeVC.swift
@@ -0,0 +1,56 @@
+//
+//  HomeVC.swift
+//  MySchool
+//
+//  Created by Etienne HERMAND on 22/07/15.
+//  Copyright (c) 2015 Etienne HERMAND. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    @IBOutlet var usernameLabel : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let isLoggedIn:Int = prefs.integerForKey("ISLOGGEDIN") as Int
+        if (isLoggedIn != 1) {
+            self.performSegueWithIdentifier("goto_login", sender: self)
+        } else {
+            self.usernameLabel.text = prefs.valueForKey("USERNAME") as? String
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // #pragma mark - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func logoutTapped(sender : UIButton) {
+        
+        let appDomain = NSBundle.mainBundle().bundleIdentifier
+        NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
+        
+        self.performSegueWithIdentifier("goto_login", sender: self)
+    }
+}
